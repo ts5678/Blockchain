ff683d5d4ef1245f3ba48a222f51157220664bae
Tim Sala <ts.5678@gmail.com>
2019-03-20 16:07:00 -0400

    planning

--- BlockchainETN/ClientApp/src/app/app.module.ts
+++ BlockchainETN/ClientApp/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { ConfigureItemComponent } from './configure-item/configure-item.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PlanningDashboardComponent } from './planning-dashboard/planning-dashboard.component';
+import { NgxLoadingModule } from 'ngx-loading';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
@@ -17,17 +20,21 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     ConfigureItemComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    PlanningDashboardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     HttpModule,
+    NgxLoadingModule.forRoot({}),
+    NgxDatatableModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'configure-item', component: ConfigureItemComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'planning-dashboard', component: PlanningDashboardComponent },
     ])
   ],
   providers: [],
---
+++ BlockchainETN/ClientApp/src/app/planning-dashboard/planning-dashboard.component.ts
@@ -0,0 +1,111 @@
+import { Component, Inject } from '@angular/core';
+import { Http, RequestOptions, RequestOptionsArgs } from '@angular/http';
+import { NgxLoadingModule } from 'ngx-loading';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { TableColumn, ColumnMode } from '@swimlane/ngx-datatable';
+
+
+@Component({
+  selector: 'planning-dashboard',
+  templateUrl: './planning-dashboard.component.html',
+  styleUrls: ['./planning-dashboard.component.css']
+})
+export class PlanningDashboardComponent {
+  //public NeedBattery: boolean = false;
+  //public OrderName: string = '';
+
+  private TheHttp: Http | null;
+  public loading: boolean = false;
+
+  public SelectedTimespan = null;
+  public Timespans = [
+    { id: 0, name: "24 Hours" },
+    { id: 2, name: "1 Week" },
+    { id: 3, name: "1 Month" }];
+
+  public rows = [];
+
+  public columns = [
+    { name: "Timestramp", prop: "Timestamp" },
+    { name: "Transaction ID", prop: 'TransactionID' },
+    { name: "Transaction Type", prop: 'TransactionType' },
+    { name: "Transaction Submitter", prop: 'TransactionSubmitter' }
+  ];
+
+  constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+    this.TheHttp = http;
+
+    this.SelectedTimespan = this.Timespans[0];
+
+    this.GetOrders();
+    //this.GetList();
+  }
+
+  public GetOrders() {
+
+    //dummy data to test table/display
+    let now = new Date();
+
+    this.rows.push(new TransactionInfo(now, Guid.newGuid(), OrderStatus.BeingBuilt, "Bob Still"));
+    this.rows.push(new TransactionInfo(now, Guid.newGuid(), OrderStatus.BeingBuilt, "Ryan Red"));
+
+    let date1 = now.setDate(now.getDate() -1);
+    this.rows.push(new TransactionInfo(new Date(date1), Guid.newGuid(), OrderStatus.OrderReceived, "Ryan Red"));
+    this.rows.push(new TransactionInfo(new Date(date1), Guid.newGuid(), OrderStatus.OrderReceived, "Bill Zed"));
+
+    let date2 = now.setDate(now.getDate() -2);
+    this.rows.push(new TransactionInfo(new Date(date2), Guid.newGuid(), OrderStatus.OrderReceived, "Fred Mikel"));
+    this.rows.push(new TransactionInfo(new Date(date2), Guid.newGuid(), OrderStatus.OrderReceived, "Bob Still"));
+    this.rows.push(new TransactionInfo(new Date(date2), Guid.newGuid(), OrderStatus.OrderReceived, "Bob Still"));
+
+    let date3 = now.setDate(now.getDate() -3);
+    this.rows.push(new TransactionInfo(new Date(date3), Guid.newGuid(), OrderStatus.Complete, "Fred Mikel"));
+    this.rows.push(new TransactionInfo(new Date(date3), Guid.newGuid(), OrderStatus.Complete, "Dun Zoe"));
+    this.rows.push(new TransactionInfo(new Date(date3), Guid.newGuid(), OrderStatus.Complete, "Dun Zoe"));
+    this.rows.push(new TransactionInfo(new Date(date3), Guid.newGuid(), OrderStatus.Complete, "Fred Mikel"));
+
+    let date4 = now.setDate(now.getDate() -10);
+    this.rows.push(new TransactionInfo(new Date(date4), Guid.newGuid(), OrderStatus.PreparingForShipping, "Bill Zed"));
+    this.rows.push(new TransactionInfo(new Date(date4), Guid.newGuid(), OrderStatus.PreparingForShipping, "Bob Still"));
+
+  }
+
+  public RunSpinner() {
+    this.loading = true;
+
+    setTimeout(() => { this.loading = false; }, 3000);
+  }
+}
+
+export class TransactionInfo {
+  public Timestamp: Date;
+  public TransactionID: string;
+  public TransactionType: OrderStatus;
+  public TransactionSubmitter: string;
+
+  //constructor() { };
+  constructor(aTimestamp: Date, aTransactionID: string, aTransactionType: OrderStatus, aTransactionSubmitter: string )
+  {
+    this.Timestamp = aTimestamp;
+    this.TransactionID = aTransactionID;
+    this.TransactionType = aTransactionType;
+    this.TransactionSubmitter = aTransactionSubmitter;
+  };
+}
+
+class Guid {
+  static newGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+}
+
+
+export const enum OrderStatus {
+  OrderReceived = "Order Received",
+  BeingBuilt = "Being Built",
+  PreparingForShipping = "Preparing For Shipping",
+  Complete = "Complete"
+}
